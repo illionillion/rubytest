5986d0de2998f4263bf7fca510ea4e894545c227
illionillion <yuto.0526.sano@gmail.com>
2021-03-18 22:26:49 +0900

    first commit

---
+++ helloworld.rb
@@ -0,0 +1,24 @@
+# puts "hello world!!"
+=begin
+x=gets
+
+x=x+"WWWW"
+
+puts x
+=end
+
+# y=10
+
+# if y>0
+# puts "#{y}は0以上"
+# end
+
+sex=0
+
+if sex==0
+  puts "男性です"
+elsif sex==1
+  puts "女性です"
+else
+  puts "不明です"
+end
