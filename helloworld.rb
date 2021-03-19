ddbf4ff2fd477ba08ef2c0973ecf339eb6b2f548
illionillion <yuto.0526.sano@gmail.com>
2021-03-19 09:12:04 +0900

    乱数と配列でおみくじ

--- helloworld.rb
+++ helloworld.rb
@@ -13,12 +13,41 @@ puts x
 # puts "#{y}は0以上"
 # end
 
-sex=0
+# sex=0
 
-if sex==0
-  puts "男性です"
-elsif sex==1
-  puts "女性です"
-else
-  puts "不明です"
-end
+# if sex==0
+#   puts "男性です"
+# elsif sex==1
+#   puts "女性です"
+# else
+#   puts "不明です"
+# end
+
+# array=[]
+# puts array
+
+# array=[1,2,3,4,5]
+# puts array
+
+# p array[1]
+
+# array.push(10)
+# array.push(13)
+
+# p array
+
+# random=rand(array.size)
+
+# p array[random]
+
+name =gets
+
+items=["大吉","中吉","小吉","吉"]
+items.push("凶")
+items.push("大凶")
+items.push("末吉")
+
+random=rand(items.size)
+
+puts name+"さんは"+items[random]+"です。"
+# p name+"さんは"+items[random]+"です。"
